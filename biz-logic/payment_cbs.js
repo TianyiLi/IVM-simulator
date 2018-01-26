7e1690ab56ca62ab046500cf395c17a68cf5a50f
PaulLi <e0991100238@gmail.com>
2018-01-26 17:35:19 +0800

    add selection

--- biz-logic/payment_cbs.js
+++ biz-logic/payment_cbs.js
@@ -7,6 +7,20 @@ module.exports.cbs = {
             return { 'e': 'reader/start' }
         }
     },
+    after_input: function (e, from, to, arg) {
+        return { e: 'payment/hint', arg: { msg: '啟動' + arg.method } }
+    },
+    after_payment_begin: function (e, from, to, arg) {
+        if (arg.method && arg.method !== 'cash') {
+            return {
+                e: 'payment/paid',
+                arg: {
+                    data: 'test',
+                    txno: arg.txno || Math.floor(new Date().valueOf() / 1000)
+                }
+            }
+        }
+    },
     enter_TRANSACTION: function (e, from, to, arg) {
         if (arg.method !== "cash")
             return { 'e': 'payment/paid' }
@@ -14,7 +28,7 @@ module.exports.cbs = {
     after_paid: function () {
         return [
             { 'e': 'dispense/goto_none' },
-            { 'e': 'dispense/start' }
+            { 'e': 'dispense/start',arg: { "mid": "product_dispensing", "msg":"完成付款，商品出貨中"} }
         ]
     },
     enter_END: function () {
--- smock-simulator/js/action.js
+++ smock-simulator/js/action.js
@@ -1,21 +1,26 @@
 ; (function (self, $) {
   'use strict'
   let disabled = true;
+  let timeStamp = function () { return Math.floor(new Date().valueOf() / 1000) }
   let events = [
     { event: "sys/sys_suspended", desc: "暫停服務", arg: '{"msg":"產品已售完，暫時停止服務"}' },
+    { event: "sys/sys_op", desc: "開始營運", arg: '{}', disabled },
     { event: "sess/timeout", desc: "閒置", arg: '{}', disabled },
     { event: "sess/session_begin", desc: "開始Session", arg: '{}', disabled },
     { event: "order/ordered", desc: "點菜p_id", arg: '{"p_id":"130"}' },
     { event: "payment/hint", desc: "支付提示", arg: '{"payment_method":{"cash":{"price":10},"easycardedc":{"price":8},"ipass":{"price":6},"isc_moneycoin":{"price":10},"isc_linepay":{"price":10}},"p_id":3,"p_name":"可口可樂","chid":"0A1"}' },
     { event: "sys/hint", desc: "開啟liveview", arg: '{"act":"liveview_ready"}' },
     { event: "sys/hint", desc: "關閉liveview", arg: '{"act":"liveview_closed"}' },
-    { event: "payment/payment_begin", desc: "投入第1個硬幣", arg: '{"paid":10, "method":"cash"}' },
+    { event: "payment/input", desc: "指定使用悠遊卡", arg: '{"method":"easycardedc"}' },
+    { event: "payment/payment_begin", desc: "投入第1個硬幣", arg: '{"paid":10, "method":"cash", "txno":' + timeStamp() + '}' },
+    { event: "payment/payment_begin", desc: "感應靠卡(悠遊卡)", arg: '{"paid":10, "method":"easycardedc", "txno":' + timeStamp() + '}' },
+    { event: "payment/payment_begin", desc: "鏡頭掃碼(任意)", arg: '{"paid":10, "method":"isc_anycode", "txno":' + timeStamp() + '}' },
     { event: "payment/hint", desc: "繼續投入硬幣", arg: '{"paid":20, "method":"cash"}' },
-    { event: "reader/read", desc: "鏡頭掃碼成功", arg: '{"paid":100, "method":"isc_alipay"}' },
-    { event: "payment/paid", desc: "交易完成", arg: '{"paid":100,"txno":' + new Date().valueOf() + '}' },
+    { event: "reader/read", desc: "鏡頭掃碼成功", arg: '{"data":"test"}' },
+    { event: "payment/paid", desc: "交易完成", arg: '{"paid":100,"txno":' + timeStamp() + '}' },
     { event: "payment/failed", desc: "支付錯誤", arg: '{"msg":"支付錯誤(305)"}' },
-    { event: "dispense/start", desc: "開始出貨", arg: '{"msg":"完成付款，商品出貨中"}' },
-    { event: "dispense/prod_dispensed", desc: "出貨成功", arg: '{"mid":"dispense_ok","msg":"ok:付货正常:00000001:00000001","elapsed":9}' }
+    { event: "dispense/prod_dispensed", desc: "出貨成功", arg: '{"mid":"dispense_ok","msg":"ok:付货正常:00000001:00000001","elapsed":9}' },
+    { event: "dispense/prod_dispensed", desc: "出貨失敗", arg: '{"mid":"dispense_ok","msg":"ok:付货正常:00000001:00000001","elapsed":9}' }
   ], _$table = $('#select-button')
 
   events.map(function (ele) {
@@ -24,6 +29,6 @@
         <td><label>${ele.desc}</label></td>
         <td><input type="text" value='${ele.arg}' ${'disabled' in ele ? 'disabled' : ''}></td>
       </tr>`).appendTo(_$table)
-    
+
   })
 })(window, jQuery);
